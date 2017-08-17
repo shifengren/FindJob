91bb2bff9c35ca5c620a9f1f7811c39e34c5c139
rsf <1944832385@qq.com>
2017-08-17 14:35:14 +0800

    add Problem "最长回文字串"

--- src/algorithm/dynamicprogram/MaximumAverageSubArray.java
+++ src/algorithm/dynamicprogram/MaximumAverageSubArray.java
@@ -1,9 +1,14 @@
 package algorithm.dynamicprogram;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 /**
  * 给定n个数的数组，找到所有长度大于等于k的连续子数组中平均值最大的那个。返回那个最大的平均值。
  * 1 <= k <= n <= 10000，数组中的元素在范围[-10000, 10000]之间，最后返回的答案的误差应在10^(-5)以内。
- *
+ * <p>
  * 题解：
  * http://www.jianshu.com/p/97c976bd2f27
  */
@@ -50,6 +55,41 @@ public class MaximumAverageSubArray {
 
         }
         return left;
+    }
+
+
+    /**
+     * 斜率优化方案O(N)
+     */
+    public double maxAverage(int[] A, int k) {
+        int[] sum = new int[A.length + 1];
+
+        for (int i = 1; i <=A.length; ++i) {
+            sum[i] = sum[i - 1] + A[i-1];
+        }
+
+
+        List<Integer> q = new ArrayList<>();
+
+        double ans = 0;
+        for (int i = k; i <= A.length; ++i) {
+            while (q.size() > 1 && isUseful(sum, q.get(q.size() - 1), q.get(q.size() - 2), i-k))
+                q.remove(q.size() - 1);
+
+            q.add(i-k);
+
+            while (q.size() > 1 && isUseful(sum, q.get(0), q.get(1), i))
+                q.remove(0);
+
+            ans = Math.max(ans, (sum[i] - sum[q.get(0)]) * 1.0 / (i - q.get(0)));
+        }
+        return ans;
+    }
+
+    private boolean isUseful(int[] sum, int ida, int idb, int idc) {
+        if ((sum[idb] - sum[ida]) * (idc - idb) <= (sum[idc] - sum[idb]) * (idb - ida))
+            return true;
+        return false;
     }
 
     public static void main(String[] args) {
